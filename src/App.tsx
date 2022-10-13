0d042ddb214010f427a7428bebb3c448e8167063
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-10-13 09:51:05 -0300

    general config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import './styles/global.css';
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1 className="text-bold text-5xl text-violet-500">Hello World!</h1>
+
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-800">
+        Enviar
+      </button>
+    </>
+  );
 };
 
 export { App };
